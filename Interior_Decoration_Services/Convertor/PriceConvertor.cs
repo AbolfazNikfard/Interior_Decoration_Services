568c4ecb3cb0ec691548a0edc8718ec4a7805391
nikfa <nikfarabolfaz@gmail.com>
2023-09-20 21:18:30 +0330

    price converter edited

--- Interior_Decoration_Services/Convertor/PriceConvertor.cs
+++ Interior_Decoration_Services/Convertor/PriceConvertor.cs
@@ -38,7 +38,7 @@
         public static int NumberOfDigit(this ulong number)
         {
             int count = 0;
-            while (number > 10)
+            while (number >= 10)
             {
                 count++;
                 number /= 10;
